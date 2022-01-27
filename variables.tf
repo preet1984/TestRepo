7c172fe116303e8c0a9b40e52268eedf09bd1074
sukhpreet <ersukhpreet@gmail.com>
2022-01-27 02:08:32 -0700

    Key removed

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "key2_cloud.pem"
-}
-
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
